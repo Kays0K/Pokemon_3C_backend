26f6a81427b9e13242d8cbf11ed1831187b0dbf7
Kays0K <kayson.games.youtub@gmail.com>
2026-03-09 11:19:27 -0300

    Console . log

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ server.put('/pokemons/:id', async (request, reply) => {
 
     const pokemonExistente = await sql.query('SELECT * FROM pokemons WHERE id = $1', [id]);
 
+    console.log(pokemonExistente);
+
     const resultado = await sql.query('UPDATE pokemons SET nome = $1, tipo = $2, nivel = $3, evolucao = $4 WHERE id = $5', [body.nome, body.tipo, body.evolucao, body.nivel, id]);
     reply.status(200).send({ message: 'Pokemon atualizado com sucesso!' });
 });
